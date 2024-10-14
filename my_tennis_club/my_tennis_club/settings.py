d1b22994853d59d093da2b6fef6cda13b52f0d1e
johngithinji-cs <mathaijohn97@gmail.com>
2024-10-14 15:42:33 +0300

    changes done

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c&2$%@=!qfy97(#zr&^4t)!0o9gbcsma^cx5vs_r=dyk9^-ze&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
